e6129d3b5b3be412a9afc6b5416797ffec310461
billaids <jimmy.nelle@hsw-stud.de>
2021-07-13 16:01:07 +0200

    hotfix: fix unclosed session issue
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- bot/helpers/search/search_service.py
+++ bot/helpers/search/search_service.py
@@ -13,17 +13,17 @@ class Pr0Items:
         self.__COOKIES = cookies
         self.__API_URL = "https://pr0gramm.com/api/items/get"
         self.__API_URL_INFO = "https://pr0gramm.com/api/items/info"
+        self.__http = aiohttp.ClientSession(cookies=self.__COOKIES)
 
     async def make_api_call(self, params: dict) -> json:
-        async with aiohttp.ClientSession(cookies=self.__COOKIES).get(
-            self.__API_URL, params=params
-        ) as resp:
+        async with self.__http.get(self.__API_URL, params=params) as resp:
             return await resp.json()
 
     async def get_tags(self, id: str or int) -> json:
-        async with aiohttp.ClientSession(cookies=self.__COOKIES).get(
-            self.__API_URL_INFO, params={"itemId": id}
-        ) as resp:
+        async with self.__http.get(self.__API_URL_INFO, params={"itemId": id}) as resp:
             response = await resp.json()
             for x in response["tags"]:
                 return re.sub(" ", "", x["tag"])
+
+    async def close(self):
+        await self.__http.close()
